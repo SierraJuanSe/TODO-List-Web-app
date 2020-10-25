fb607cc6273b7587f67fbc2028d4a316f3b796ab
Felipe <luis.velasquez01@correo.usa.edu.co>
2020-10-24 21:20:47 -0500

    Crear To_DO

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// $("#wrapper").hide();
 $("#Cuenta").click(function () {
     // Animacion para mostrara el regiitro:
     $("#Registro").removeClass("animate__animated animate__backOutRight");
@@ -25,19 +26,19 @@ $("#CrearCuenta").click(function () {
     contraseña1 = $("#verifypass").val();
     //Verifica la contraseña
     if (contraseña == contraseña1) {
-        cor=crearCuenta(nombre,apellido,correo,contraseña);
-        if (cor==true) {
+        cor = crearCuenta(nombre, apellido, correo, contraseña);
+        if (cor == true) {
             //alerta correcta
-        swal("Cuenta creada correctamente", "Estas listo para administrar tus tareas", "success")
-            .then((value) => {
-                // Animacion de creacion de usuario
-                $("#Registro").removeClass("animate__animated animate__backInRight");
-                $("#login").removeClass("animate__backOutLeft");
-                $("#Registro").addClass("animate__animated animate__backOutRight");
-                $("#login").addClass("animate__animated animate__backInLeft");
-            });
+            swal("Cuenta creada correctamente", "Estas listo para administrar tus tareas", "success")
+                .then((value) => {
+                    // Animacion de creacion de usuario
+                    $("#Registro").removeClass("animate__animated animate__backInRight");
+                    $("#login").removeClass("animate__backOutLeft");
+                    $("#Registro").addClass("animate__animated animate__backOutRight");
+                    $("#login").addClass("animate__animated animate__backInLeft");
+                });
             //alerta incorrecta
-        }else{
+        } else {
             swal("Error", "Revisa si ingrsaste los datos correctamente", "error");
         }
     } else {
@@ -51,28 +52,69 @@ $("#Ingresar").click(function () {
     //Lectura de datos
     correo = $("#emailLogin").val();
     contraseña = $("#passwordLogin").val();
-    if (correo=="" || contraseña=="") { //valida que no sen nulos
+    if (correo == "" || contraseña == "") { //valida que no sen nulos
         //Envio de alerta
         $("#alertaLogin").append('<div class="alert alert-danger" role="alert">Error, por favor ingresa todo los datos</div>')
 
     } else {
         //Invoca a metodo de peticion
-        if (login(correo,contraseña)) {
-        mostrarPaginaPrincipal();
-        }else{
+        if (login(correo, contraseña)) {
+            mostrarPaginaPrincipal();
+        } else {
             //Envio de alerta
             $("#alertaLogin").append('<div class="alert alert-danger" role="alert">Error, intentalo nuevamente</div>')
         }
     }
-  
+
+});
+
+$("#cerrarsesion").click(function () {
+    $("#fondo").removeClass("animate__animated animate__backOutLeft");
+    $("#wrapper").removeClass("animate__animated animate__backInRight");
+    $("#wrapper").hide(700);
+    $('#fondo').show(700);
 });
 
 //Boton para habilitar el menu Hamburguesa
-    $('#menu_on').click(function(){
-    	$('body').toggleClass('visible_menu');
-    })
+$('#menu_on').click(function () {
+    $('body').toggleClass('visible_menu');
+})
+
+$("#botonCrearTodo").click(function () {
+    $('#nuevot').hide(700);
+    $("#crearT").show(700);
+});
+
+$("#Creartodo").click(function () {
+    $('#nuevot').
+        titulo = $('#titulo').val();
+    des = $('#Descripcion').val();
+    Fecha = $('#fechalimite').val();
+    if (titulo == "" || des == "") {
+        swal("Error", "Ingresas todos los datos", "error");
+    } else {
+        if (crearTodo(titulo, des, Fecha, true)) {
+            swal("Excelente", "Creaste un To-Do correctamente", "success")
+                .then((value) => {
+                    $('#nuevot').show(700);
+                    $("#crearT").hide(700);
+                });
+
+        } else {
+            swal("Error", "Revisa si ingrsaste los datos correctamente", "error");
+        }
+    }
+});
+
+$("#cancelartodo").click(function () {
+    $('#nuevot').show(700);
+    $("#crearT").hide(700);
+});
 
 
-function mostrarPaginaPrincipal(){
-    
+function mostrarPaginaPrincipal() {
+    $('#fondo').hide(700);
+    $("#fondo").addClass("animate__animated animate__backOutLeft");
+    $("#wrapper").addClass("animate__animated animate__backInRight");
+    $("#wrapper").show();
 }
--- static/js/peticiones.js
+++ static/js/peticiones.js
@@ -5,3 +5,19 @@ function crearCuenta(nombre,apellido,correo,contraseña) {
 function login(correo,contraseña) {
     return true;
 }
+
+function consultarTodo() {
+    return 0;
+}
+
+function crearTodo(titulo,descripcion,fechaCreacion,estado) {
+    return true;
+}
+
+function BorrarTodo(codigoTodo) {
+    return true;
+}
+
+function actualizarTOdo() {
+    return true;
+}
